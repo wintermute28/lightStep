fa037dcae1afe0b9769e2afd6e33d62a9a0e0ef2
wintermute28 <a28c@yandex.ru>
2024-04-21 18:00:02 +0300

    Add feedback and favicon

--- js/main.js
+++ js/main.js
@@ -68,3 +68,21 @@ $(document).ready(function() {
 		// $('body').removeClass('no-scroll');
 	});
 });
+
+
+
+
+let acc = document.getElementsByClassName("accordionn");
+let i;
+ 
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
